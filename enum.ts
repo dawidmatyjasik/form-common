70a738cc718b4bd0c8d26796f0ed38aea6c57d95
Dawid Spisak <63721806+stairss@users.noreply.github.com>
2022-10-17 22:00:06 +0200

    init

---
+++ enum.ts
@@ -0,0 +1,16 @@
+export const enum personalEnum {
+    lastName = "lastName",
+    firstName =  "firstName",
+    pesel = "pesel",
+    dateOfBirth ="dateOfBirth",
+    placeOfBirth ="placeOfBirth",
+    gender ="gender",
+    phoneNumber ="phoneNumber",
+    zipCode ="zipCode",
+    city ="city",
+    street ="street",
+    streetNumber ="streetNumber",
+    blackList = 'blackList',
+    blackListReason = 'blackListReason',
+    assignedProject = 'assignedProject'
+}

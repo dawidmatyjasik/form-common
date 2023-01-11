42b04852b2e6c5a312f5cafcbed0c83895304355
Lefju <karol.legut121@gmail.com>
2023-01-11 19:20:33 +0100

    feat: handle routes

---
+++ routes.js
@@ -0,0 +1,11 @@
+const PERSONAL_ROUTES = Object.freeze({
+  GET: "/personal",
+  POST: "/personal/:id",
+  GET_ALL: "/personal/all",
+  DELETE: "/personal/delete/:id",
+  UPDATE: "/personal/update/:id",
+});
+
+const ROUTES = Object.freeze({
+  PERSONAL: PERSONAL_ROUTES,
+});

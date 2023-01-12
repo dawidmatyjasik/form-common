b875cc8bcfe8fb1d18a2f13dc824e7b3d0ebccd6
Dawid Spisak <63721806+stairss@users.noreply.github.com>
2023-01-12 20:53:20 +0100

    fix: refactor code

--- personal/validator.ts
+++ personal/validator.ts
@@ -1,40 +1,45 @@
-import { date, object, TypeOf, string, number, boolean } from "zod";
+import { date, object, TypeOf, string, number } from "zod";
 import { personalEnum } from "../enum";
 
 export const personalValidator = object({
-  [personalEnum.last_name]: string(),
-  [personalEnum.first_name]: string(),
-  [personalEnum.pesel]: string(),
-  [personalEnum.date_of_birth]: date(),
-  [personalEnum.place_of_birth]: string(),
-  [personalEnum.gender]: string(),
-  [personalEnum.phone_number]: string(),
-  [personalEnum.email]: string().email("err"),
-  [personalEnum.street]: string(),
-  [personalEnum.street_number]: string(),
-  [personalEnum.apartment_number]: string(),
-  [personalEnum.city]: string(),
-  [personalEnum.zip_code]: string(),
-  [personalEnum.county]: string(),
-  [personalEnum.community]: string(),
-  [personalEnum.voivodeship]: string(),
-  [personalEnum.mailing_address]: string(),
-  [personalEnum.mailing_street]: string(),
-  [personalEnum.mailing_street_number]: string(),
-  [personalEnum.mailing_apartment_number]: string(),
-  [personalEnum.mailing_city]: string(),
-  [personalEnum.mailing_zip_code]: string(),
-  [personalEnum.mailing_county]: string(),
-  [personalEnum.mailing_community]: string(),
-  [personalEnum.mailing_voivodeship]: string(),
-  [personalEnum.black_list]: string(),
-  [personalEnum.marketing_agreement]: string(),
-  [personalEnum.shiping_agreement]: string(),
-  [personalEnum.assigned_project]: string(),
-  [personalEnum.sub_region]: string(),
-  [personalEnum.osi]: string(),
-  [personalEnum.medium_city]: string(),
-  [personalEnum.rural_area]: string(),
+  [personalEnum.last_name]: string().optional(),
+  [personalEnum.first_name]: string().optional(),
+  [personalEnum.pesel]: string()
+    .length(11, "Numer pesel jest nieprawidłowy")
+    .optional(),
+  [personalEnum.date_of_birth]: date().optional(),
+  [personalEnum.place_of_birth]: string().optional(),
+  [personalEnum.gender]: number().optional(),
+  [personalEnum.phone_number]: string().trim().optional(),
+  [personalEnum.email]: string()
+    .email("Adres email jest nieprawidłowy")
+    .optional(),
+  [personalEnum.street]: string().optional(),
+  [personalEnum.street_number]: string().optional(),
+  [personalEnum.apartment_number]: string().optional(),
+  [personalEnum.city]: string().optional(),
+  [personalEnum.zip_code]: string().optional(),
+  [personalEnum.county]: string().optional(),
+  [personalEnum.community]: string().optional(),
+  [personalEnum.voivodeship]: string().optional(),
+  [personalEnum.mailing_address]: number().optional(),
+  [personalEnum.mailing_street]: string().optional(),
+  [personalEnum.mailing_street_number]: string().optional(),
+  [personalEnum.mailing_apartment_number]: string().optional(),
+  [personalEnum.mailing_city]: string().optional(),
+  [personalEnum.mailing_zip_code]: string().optional(),
+  [personalEnum.mailing_county]: string().optional(),
+  [personalEnum.mailing_community]: string().optional(),
+  [personalEnum.mailing_voivodeship]: string().optional(),
+  [personalEnum.black_list]: number().optional(),
+  [personalEnum.marketing_agreement]: number().optional(),
+  [personalEnum.shiping_agreement]: number().optional(),
+  [personalEnum.assigned_project]: number().optional(),
+  [personalEnum.sub_region]: number().optional(),
+  [personalEnum.osi]: number().optional(),
+  [personalEnum.zit]: number().optional(),
+  [personalEnum.medium_city]: number().optional(),
+  [personalEnum.rural_area]: number().optional(),
 });
 
 /* 
@@ -44,4 +49,4 @@ export const personalValidator = object({
 }); 
 */
 
-export type ILogin = TypeOf<typeof personalValidator>;
+export type IPersonal = TypeOf<typeof personalValidator>;

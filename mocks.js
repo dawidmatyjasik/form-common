7211d383ee0f772a834bc477d72d171e45530b93
Lefju <karol.legut121@gmail.com>
2023-01-29 20:20:03 +0100

    fix: change from arrays to objects

--- mocks.js
+++ mocks.js
@@ -1,76 +1,101 @@
-export const gender = ["Mężczyzna", "Kobieta", "Neutralny"];
+export const gender = {
+  1: "Mężczyzna",
+  2: "Kobieta",
+  3: "Neutralny",
+};
 
-export const choice = ["Nie", "Tak"];
+export const choice = {
+  0: "Nie",
+  1: "Tak",
+};
 
-export const choiceRefusal = ["Nie", "Tak", "Omowa podania informacji"];
+export const choiceRefusal = {
+  0: "Nie",
+  1: "Tak",
+  2: "Omowa podania informacji",
+};
 
-export const choiceApply = ["Nie", "Tak", "Nie dotyczy"];
+export const choiceApply = {
+  0: "Nie",
+  1: "Tak",
+  2: "Nie dotyczy",
+};
 
-export const projects = ["Projekt1", "Projekt2", "Projekt3"];
+export const projects = {
+  1: "Projekt1",
+  2: "Projekt2",
+  3: "Projekt3",
+};
 
-export const mailing = ["Jak zamieszkania", "Inny"];
+export const mailing = {
+  1: "Jak zamieszkania",
+  2: "Inny",
+};
 
-export const education = [
-  "Podstawowe (ISCED 1)",
-  "Gimnazjalne (ISCED 2)",
-  "Ponadgimnazjalne - liceum, technikum, szkoła zawodowa (ISCED 3)",
-  "Policealne (ISCED 4)",
-  "Wyższe (ISCED 5-8)",
-];
+export const education = {
+  1: "Podstawowe (ISCED 1)",
+  2: "Gimnazjalne (ISCED 2)",
+  3: "Ponadgimnazjalne - liceum, technikum, szkoła zawodowa (ISCED 3)",
+  4: "Policealne (ISCED 4)",
+  5: "Wyższe (ISCED 5-8)",
+};
 
-export const disability = [
-  "Brak",
-  "Stopień lekki",
-  "Stopień umiarkowany",
-  "Stopień znaczny",
-  "Odmowa podania informacji",
-];
+export const disability = {
+  1: "Brak",
+  2: "Stopień lekki",
+  3: "Stopień umiarkowany",
+  4: "Stopień znaczny",
+  5: "Odmowa podania informacji",
+};
 
-export const disability_level = [
-  "Orzeczenie o niepełnosprawności",
-  "Orzeczenie lek. ZUS",
-  "Orzeczenie lek. KRUS",
-  "Orzeczenie lub opinia o stanie zdrowia psychicznego",
-];
+export const disability_level = {
+  1: "Orzeczenie o niepełnosprawności",
+  2: "Orzeczenie lek. ZUS",
+  3: "Orzeczenie lek. KRUS",
+  4: "Orzeczenie lub opinia o stanie zdrowia psychicznego",
+};
 
-export const working_status = [
-  "Osoba pracująca",
-  "Osoba bezrobotna",
-  "Osoba bierna zawodowo",
-  "Outplacment",
-];
+export const working_status = {
+  1: "Osoba pracująca",
+  2: "Osoba bezrobotna",
+  3: "Osoba bierna zawodowo",
+  4: "Outplacment",
+};
 
-export const working = [
-  "Ubogo pracująca",
-  "Umowa cywilnoprawna",
-  "Umowa krótkoterminowa",
-  "Działalność gospdarcza",
-  "Umowa o pracę",
-];
+export const working = {
+  1: "Ubogo pracująca",
+  2: "Umowa cywilnoprawna",
+  3: "Umowa krótkoterminowa",
+  4: "Działalność gospdarcza",
+  5: "Umowa o pracę",
+};
 
-export const working_type = [
-  "Administracja rządowa",
-  "Administracja samorządowa",
-  "Duże przedsiębiorstwo",
-  "MMŚP",
-  "Organizacja pozarządowa",
-  "Jednosoobowa działalność gospodarcza",
-];
+export const working_type = {
+  1: "Administracja rządowa",
+  2: "Administracja samorządowa",
+  3: "Duże przedsiębiorstwo",
+  4: "MMŚP",
+  5: "Organizacja pozarządowa",
+  6: "Jednosoobowa działalność gospodarcza",
+};
 
-export const working_classification = [
-  "Instruktor praktyczenj nauki zawodu",
-  "Kluczowy pracownik instytucji pomocy i integracji społecznej",
-  "Nauczyciel kształcenia ogólnego",
-  "Nauczyciel kształcenia zawodowego",
-  "Nauczyciel wychowania przedszkolnego",
-  "Pracownik insytucji rynku pracy",
-  "Pracownik insystucji systemu ochrony zdrowia",
-  "Pracownik instytucji systemu wspierania rodzin i pieczy zastępczej",
-  "Pracownik instytucji szkolnictwa wyższego",
-  "Pracownik wsparcia ekonomii społecznej",
-  "Pracownik poradni psychologiczno-pedagogicznej",
-  "Rolnik",
-  "Inne",
-];
+export const working_classification = {
+  1: "Instruktor praktyczenj nauki zawodu",
+  2: "Kluczowy pracownik instytucji pomocy i integracji społecznej",
+  3: "Nauczyciel kształcenia ogólnego",
+  4: "Nauczyciel kształcenia zawodowego",
+  5: "Nauczyciel wychowania przedszkolnego",
+  6: "Pracownik insytucji rynku pracy",
+  7: "Pracownik insystucji systemu ochrony zdrowia",
+  8: "Pracownik instytucji systemu wspierania rodzin i pieczy zastępczej",
+  9: "Pracownik instytucji szkolnictwa wyższego",
+  10: "Pracownik wsparcia ekonomii społecznej",
+  11: "Pracownik poradni psychologiczno-pedagogicznej",
+  12: "Rolnik",
+  13: "Inne",
+};
 
-export const unemployment_type = ["Niezarejestrowana", "Zarejestrowana w UP"];
+export const unemployment_type = {
+  1: "Niezarejestrowana",
+  2: "Zarejestrowana w UP",
+};

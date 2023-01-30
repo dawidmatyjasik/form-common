cf2ddbfccd2c3dce20879ce280d4fedc0fc52909
Lefju <karol.legut121@gmail.com>
2023-01-30 12:53:24 +0100

    fix: date validation fix

--- personal/validator.js
+++ personal/validator.js
@@ -1,5 +1,8 @@
 import { personalEnum } from "../enum.js";
 
+const dataPattern =
+  "^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9]).[0-9]{3}Z$";
+
 export const validator = {
   type: "object",
   required: ["last_name", "first_name"],
@@ -8,9 +11,9 @@ export const validator = {
     [personalEnum.first_name]: { type: "string", minLength: 3 },
     [personalEnum.pesel]: { type: "string", minLength: 11, maxLength: 11 },
     [personalEnum.date_of_birth]: {
+      type: "string",
       format: "date-time",
-      pattern:
-        "^[0-9]{4}-((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01])|(0[469]|11)-(0[1-9]|[12][0-9]|30)|(02)-(0[1-9]|[12][0-9]))T(0[0-9]|1[0-9]|2[0-3]):(0[0-9]|[1-5][0-9]):(0[0-9]|[1-5][0-9]).[0-9]{3}Z$",
+      pattern: dataPattern,
     },
     [personalEnum.place_of_birth]: { type: "string" },
     [personalEnum.gender]: { type: "integer", enum: [1, 2, 3] },
@@ -30,7 +33,7 @@ export const validator = {
     [personalEnum.county]: { type: "string" },
     [personalEnum.community]: { type: "string" },
     [personalEnum.voivodeship]: { type: "string" },
-    [personalEnum.mailing_address]: { type: "integer", enum: [0, 1] },
+    [personalEnum.mailing_address]: { type: "integer", enum: [1, 2] },
     [personalEnum.mailing_street]: { type: "string" },
     [personalEnum.mailing_street_number]: { type: "string" },
     [personalEnum.mailing_apartment_number]: { type: "string" },
--- routes.js
+++ routes.js
@@ -6,6 +6,24 @@ const PERSONAL_ROUTES = Object.freeze({
   UPDATE: "/personal/update",
 });
 
+const PARTICIPANT_ROUTES = Object.freeze({
+  GET: "/participant/get",
+  POST: "/participant/post",
+  GET_ALL: "/participant/getAll",
+  DELETE: "/participant/delete",
+  UPDATE: "/participant/update",
+});
+
+// const TRAINING_ROUTES = Object.freeze({
+//   GET: "/training/get",
+//   POST: "/training/post",
+//   GET_ALL: "/training/getAll",
+//   DELETE: "/training/delete",
+//   UPDATE: "/training/update",
+// });
+
 export const ROUTES = Object.freeze({
   PERSONAL: PERSONAL_ROUTES,
+  PARTICIPANT: PARTICIPANT_ROUTES,
+  // TRAINING: TRAINING_ROUTES,
 });

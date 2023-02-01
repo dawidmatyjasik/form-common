643278aa46588f64e69d2e0e3bf337e621ccbb42
Lefju <karol.legut121@gmail.com>
2023-02-01 01:01:53 +0100

    feat: change of structure mocks

--- mocks.js
+++ mocks.js
@@ -55,47 +55,103 @@ export const disability_level = {
   4: "Orzeczenie lub opinia o stanie zdrowia psychicznego",
 };
 
-export const working_status = {
-  1: "Osoba pracująca",
-  2: "Osoba bezrobotna",
-  3: "Osoba bierna zawodowo",
-  4: "Outplacment",
+export const workingStatus = {
+  working: 1,
+  unemployed: 2,
+  professionally_inactive: 3,
+  outplacement: 4,
+};
+
+export const workingStatusEnum = {
+  [workingStatus.working]: "Osoba pracująca",
+  [workingStatus.unemployed]: "Osoba pracująca",
+  [workingStatus.professionally_inactive]: "Osoba pracująca",
+  [workingStatus.outplacement]: "Osoba pracująca",
 };
 
 export const working = {
-  1: "Ubogo pracująca",
-  2: "Umowa cywilnoprawna",
-  3: "Umowa krótkoterminowa",
-  4: "Działalność gospdarcza",
-  5: "Umowa o pracę",
+  working_poor: 1,
+  civil_law: 2,
+  short_term: 3,
+  economic_activity: 4,
+  contract_of_employment: 5,
 };
 
-export const working_type = {
-  1: "Administracja rządowa",
-  2: "Administracja samorządowa",
-  3: "Duże przedsiębiorstwo",
-  4: "MMŚP",
-  5: "Organizacja pozarządowa",
-  6: "Jednosoobowa działalność gospodarcza",
+export const workingEnum = {
+  [working.working_poor]: "Ubogo pracująca",
+  [working.civil_law]: "Umowa cywilnoprawna",
+  [working.short_term]: "Umowa krótkoterminowa",
+  [working.economic_activity]: "Działalność gospdarcza",
+  [working.contract_of_employment]: "Umowa o pracę",
 };
 
-export const working_classification = {
-  1: "Instruktor praktyczenj nauki zawodu",
-  2: "Kluczowy pracownik instytucji pomocy i integracji społecznej",
-  3: "Nauczyciel kształcenia ogólnego",
-  4: "Nauczyciel kształcenia zawodowego",
-  5: "Nauczyciel wychowania przedszkolnego",
-  6: "Pracownik insytucji rynku pracy",
-  7: "Pracownik insystucji systemu ochrony zdrowia",
-  8: "Pracownik instytucji systemu wspierania rodzin i pieczy zastępczej",
-  9: "Pracownik instytucji szkolnictwa wyższego",
-  10: "Pracownik wsparcia ekonomii społecznej",
-  11: "Pracownik poradni psychologiczno-pedagogicznej",
-  12: "Rolnik",
-  13: "Inne",
+export const workingType = {
+  government_administration: 1,
+  local_government: 2,
+  large_enterprise: 3,
+  mmsp: 4,
+  non_governmental_organization: 5,
+  sole_proprietorship: 6,
 };
 
-export const unemployment_type = {
-  1: "Niezarejestrowana",
-  2: "Zarejestrowana w UP",
+export const workingTypeEnum = {
+  [workingType.government_administration]: "Administracja rządowa",
+  [workingType.local_government]: "Administracja samorządowa",
+  [workingType.large_enterprise]: "Duże przedsiębiorstwo",
+  [workingType.mmsp]: "MMŚP",
+  [workingType.non_governmental_organization]: "Organizacja pozarządowa",
+  [workingType.sole_proprietorship]: "Jednosoobowa działalność gospodarcza",
+};
+
+export const workingClassification = {
+  practical_training_instructor: 1,
+  key_employee: 2,
+  general_education_teacher: 3,
+  vocational_education_teacher: 4,
+  preschool_education_teacher: 5,
+  labor_market_institutions: 6,
+  health_care_system_institution: 7,
+  family_support_system_and_foster_care: 8,
+  higher_education_institution: 9,
+  social_economy_support: 10,
+  psychological_and_pedagogical_counseling_center: 11,
+  farmer: 12,
+  other: 13,
+};
+
+export const workingClassificationEnum = {
+  [workingClassification.practical_training_instructor]:
+    "Instruktor praktyczenj nauki zawodu",
+  [workingClassification.key_employee]:
+    "Kluczowy pracownik instytucji pomocy i integracji społecznej",
+  [workingClassification.general_education_teacher]:
+    "Nauczyciel kształcenia ogólnego",
+  [workingClassification.vocational_education_teacher]:
+    "Nauczyciel kształcenia zawodowego",
+  [workingClassification.preschool_education_teacher]:
+    "Nauczyciel wychowania przedszkolnego",
+  [workingClassification.labor_market_institutions]:
+    "Pracownik insytucji rynku pracy",
+  [workingClassification.health_care_system_institution]:
+    "Pracownik insystucji systemu ochrony zdrowia",
+  [workingClassification.family_support_system_and_foster_care]:
+    "Pracownik instytucji systemu wspierania rodzin i pieczy zastępczej",
+  [workingClassification.higher_education_institution]:
+    "Pracownik instytucji szkolnictwa wyższego",
+  [workingClassification.social_economy_support]:
+    "Pracownik wsparcia ekonomii społecznej",
+  [workingClassification.psychological_and_pedagogical_counseling_center]:
+    "Pracownik poradni psychologiczno-pedagogicznej",
+  [workingClassification.farmer]: "Rolnik",
+  [workingClassification.other]: "Inne",
+};
+
+export const unemploymentType = {
+  unregistered: 1,
+  registered_at_up: 2,
+};
+
+export const unemploymentTypeEnum = {
+  [unemploymentType.unregistered]: "Niezarejestrowana",
+  [unemploymentType.registered_at_up]: "Zarejestrowana w UP",
 };
